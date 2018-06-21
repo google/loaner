a2f7db498c62ab52b441c6fec0ea91cda01fa32d
Mike Helfrich <me@michaelhelfrich.com>
2018-06-21 10:56:21 -0700

    Prevents unbuildable code from allowing tests to pass.
    PiperOrigin-RevId: 201551578

--- angular_tests.sh
+++ angular_tests.sh
@@ -6,5 +6,19 @@
 set -ex
 
 cd loaner
+
+# Install node modules.
 npm install
+
+# Run frontend build.
+npm run build:frontend
+# Cleanup compiled files to prevent the disk from filling.
+rm -r web_app/frontend/dist
+
+# Run chrome app build.
+npm run build:chromeapp:once
+# Cleanup compiled files to prevent the disk from filling.
+rm -r chrome_app/dist
+
+# Run chrome app and frontend tests.
 npm run test:once
