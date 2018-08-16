df09880183424329b6dfa4944929605cd302523c
Adriano Tressino <adriano.tressino@gmail.com>
2018-08-16 10:07:37 -0700

    Create DeviceActionsView tests.
    PiperOrigin-RevId: 209002101

--- loaner/web_app/frontend/src/views/device_actions_view/device_actions_view_test.ts
+++ loaner/web_app/frontend/src/views/device_actions_view/device_actions_view_test.ts
@@ -12,3 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {ComponentFixture, fakeAsync, flushMicrotasks, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+
+import {DeviceService} from '../../services/device';
+import {DeviceServiceMock} from '../../testing/mocks';
+
+import {DeviceActionsView, DeviceActionsViewModule} from '.';
+
+describe('DeviceActionsView', () => {
+  let fixture: ComponentFixture<DeviceActionsView>;
+  let deviceActionsView: DeviceActionsView;
+
+  beforeEach(fakeAsync(() => {
+    TestBed
+        .configureTestingModule({
+          imports: [RouterTestingModule, DeviceActionsViewModule],
+          providers: [
+            {provide: DeviceService, useClass: DeviceServiceMock},
+          ],
+        })
+        .compileComponents();
+
+    flushMicrotasks();
+
+    fixture = TestBed.createComponent(DeviceActionsView);
+    deviceActionsView = fixture.debugElement.componentInstance;
+  }));
+
+  it('should create the deviceActionsView', () => {
+    expect(deviceActionsView).toBeDefined();
+  });
+
+  it('renders the device header title in a mat-card-title', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.mat-card-title').innerText)
+        .toContain('Device List');
+  });
+
+  it('renders the device enroll/unenroll list', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(
+        compiled.getElementsByTagName('loaner-device-enroll-unenroll-list')[0])
+        .toBeDefined();
+  });
+});
