626e3b516305371ac67b8d53c4a6c9221bb219fb
Joe Leuenberger <joe@jplabs.org>
2018-08-17 05:38:20 -0700

    Add the Google Admin SDK Directory API client library for deployments.
    PiperOrigin-RevId: 209136931

---
+++ loaner/deployments/lib/directory.py
@@ -0,0 +1,207 @@
+# Copyright 2018 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This library provides access to the Google Admin Directory API."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import httplib
+
+from absl import logging
+
+from googleapiclient import errors
+
+from loaner.deployments.lib import auth
+
+# Google App Engine Admin SDK constants.
+_SERVICE_NAME = 'admin'
+_VERSION = 'directory_v1'
+_SCOPES = (
+    'https://www.googleapis.com/auth/admin.directory.rolemanagement',
+    'https://www.googleapis.com/auth/admin.directory.user',
+)
+
+# Error messages.
+_FORBIDDEN_ERROR_MSG = (
+    'Failed to contact the Google Admin Directory API, please ensure API access'
+    ' is enabled. Instructions can be found here: '
+    'https://support.google.com/a/answer/60757?hl=en')
+_INSERT_ROLE_ERROR_MSG = (
+    'Failed to create new Google Admin Role with name %r: %r.')
+_INSERT_USER_ERROR_MSG = 'Failed to create new GSuite User with email %r: %r.'
+
+# Default privileges for the GSuite Role Account.
+_ROLE_PRIVILEGES = [
+    {'privilegeName': 'USERS_RETRIEVE', 'serviceId': '00haapch16h1ysv'},
+    {'privilegeName': 'MANAGE_DEVICES', 'serviceId': '04f1mdlm0ki64aw'},
+    {'privilegeName': 'ORGANIZATION_UNITS_CREATE',
+     'serviceId': '00haapch16h1ysv'},
+    {'privilegeName': 'GROUPS_RETRIEVE', 'serviceId': '00haapch16h1ysv'},
+]
+
+# Default GSuite Admin Role description.
+_ROLE_DESCRIPTION = (
+    'Grab n Go Admin Role to allow access to the Google Admin Directory API.')
+
+# Default GSuite Admin Role name.
+_ROLE_NAME = 'Grab n Go Admin'
+
+# Defaults for the role account to create and use in GSuite.
+_DEFAULT_ROLE_FAMILY_NAME = 'Role Account'
+_DEFAULT_ROLE_GIVEN_NAME = 'Grab n Go'
+_PRIMARY_EMAIL = 'grab-n-go-role@example.com'
+_ORG_UNIT = '/'
+
+
+class AlreadyExistsError(Exception):
+  """Raised when a resource already exists."""
+
+
+class ForbiddenError(Exception):
+  """Raised when unable to contact the Google Admin Directory API."""
+
+
+class InsertionError(Exception):
+  """Raised when the creation of a resource fails."""
+
+
+class NotFoundError(Exception):
+  """Raised when a resource is not found."""
+
+
+class DirectoryAPI(object):
+  """Google Admin Directory API access."""
+
+  def __init__(self, config, client):
+    """Initializes the Google Admin Directory API.
+
+    This object should be initialized using the classmethod 'from_config'.
+
+    Args:
+      config: common.ProjectConfig, the project configuration.
+      client: the client to use when accessing the Google Admin Directory API.
+    """
+    logging.debug('Creating a new DirectoryAPI object with config: %s', config)
+    self._config = config
+    self._client = client
+
+  def __str__(self):
+    return '{} for project: {}.'.format(
+        self.__class__.__name__, self._config.project)
+
+  @classmethod
+  def from_config(cls, config):
+    """Returns an initialized DirectoryAPI object.
+
+    Args:
+      config: common.ProjectConfig, the project configuration.
+
+    Returns:
+      An authenticated Google Admin Directory API object.
+    """
+    creds = auth.CloudCredentials(config, _SCOPES)
+    client = creds.get_api_client(_SERVICE_NAME, _VERSION, _SCOPES)
+    return cls(config, client)
+
+  def insert_role(self, name=_ROLE_NAME):
+    """Creates and inserts a new GSuite Admin Role.
+
+    Args:
+      name: str, the name of the new GSuite Admin Role.
+
+    Returns:
+      A dictionary object representing the new GSuite Admin Role.
+          https://developers.google.com/admin-sdk/directory/v1/reference/roles
+
+    Raises:
+      AlreadyExistsError: when the role with the provided name already exists.
+      ForbiddenError: when authorization fails.
+      InsertionError: when creation fails (e.g. failed to authenticate, improper
+          scopes, etc).
+    """
+    try:
+      return self._client.roles().insert(
+          customer='my_customer',
+          body={
+              'roleName': name,
+              'rolePrivileges': _ROLE_PRIVILEGES,
+              'roleDescription': _ROLE_DESCRIPTION,
+              'isSystemRole': False,
+              'isSuperAdminRole': False,
+          },
+      ).execute()
+    except errors.HttpError as err:
+      status = err.resp.status
+
+      if status == httplib.CONFLICT or status == httplib.INTERNAL_SERVER_ERROR:
+        raise AlreadyExistsError('Role with name %r already exists.' % name)
+
+      if status == httplib.FORBIDDEN:
+        raise ForbiddenError(_FORBIDDEN_ERROR_MSG)
+
+      logging.error(_INSERT_ROLE_ERROR_MSG, name, err)
+      raise InsertionError(_INSERT_ROLE_ERROR_MSG % (name, err))
+
+  def insert_user(
+      self, password, family_name=_DEFAULT_ROLE_FAMILY_NAME,
+      given_name=_DEFAULT_ROLE_GIVEN_NAME, primary_email=_PRIMARY_EMAIL,
+      org_unit=_ORG_UNIT):
+    """Creates and inserts a new Google Admin User.
+
+    Args:
+      password: str, the password to associate with the new GSuite user.
+      family_name: str, the family name of the GSuite user to insert.
+      given_name: str, the given name of the GSuite user to insert.
+      primary_email: str, the email address of the GSuite user to insert.
+      org_unit: str, the path to the Organizational Unit where the new GSuite
+          user should be inserted.
+
+    Returns:
+      A dictionary object representing the new GSuite user.
+          https://developers.google.com/admin-sdk/directory/v1/reference/users
+
+    Raises:
+      AlreadyExistsError: when the user with the provided email address already
+          exists.
+      ForbiddenError: when authorization fails.
+      InsertionError: when creation fails (e.g. failed to authenticate, improper
+          scopes, etc).
+    """
+    try:
+      return self._client.users().insert(
+          body={
+              'name': {
+                  'familyName': family_name,
+                  'givenName': given_name,
+              },
+              'primaryEmail': primary_email,
+              'password': password,
+              'orgUnitPath': org_unit,
+              'changePasswordAtNextLogin': False,
+          },
+      ).execute()
+    except errors.HttpError as err:
+      status = err.resp.status
+
+      if status == httplib.CONFLICT or status == httplib.INTERNAL_SERVER_ERROR:
+        raise AlreadyExistsError(
+            'User with email address %r already exists.' % primary_email)
+
+      if status == httplib.FORBIDDEN:
+        raise ForbiddenError(_FORBIDDEN_ERROR_MSG)
+
+      logging.error(_INSERT_USER_ERROR_MSG, primary_email, err)
+      raise InsertionError(_INSERT_USER_ERROR_MSG % (primary_email, err))
---
+++ loaner/deployments/lib/directory_test.py
@@ -0,0 +1,140 @@
+# Copyright 2018 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for deployments.lib.directory."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import httplib
+
+from absl.testing import parameterized
+
+from googleapiclient import errors
+
+import httplib2
+import mock
+
+from loaner.deployments.lib import auth
+from loaner.deployments.lib import common
+from loaner.deployments.lib import directory
+from absl.testing import absltest
+
+_FAKE_ROLE_OBJECT = {
+    'kind': 'admin#directory#role',
+    'etag': 'E_TAG',
+    'roleId': 'ROLE_ID',
+    'roleName': 'ROLE_NAME',
+    'rolePrivileges': [{
+        'privilegeName': 'PRIVILEGE_NAME_0', 'serviceId': 'SERVICE_ID_0',
+    }],
+}
+
+_FAKE_USER_OBJECT = {
+    'kind': 'admin#directory#user',
+    'id': 'unique_id',
+    'etag': 'E_TAG',
+    'primaryEmail': 'grab-n-go-role@example.com',
+    'name': {
+        'givenName': 'Grab n Go',
+        'familyName': 'Role Account',
+    },
+    'isAdmin': False,
+    'isDelegatedAdmin': False,
+    'creationTime': '2018-01-01T00:00:00.000Z',
+    'changePasswordAtNextLogin': False,
+    'customerId': 'my_customer',
+    'orgUnitPath': '/',
+    'isMailboxSetup': False,
+}
+
+
+class DirectoryAPITest(parameterized.TestCase, absltest.TestCase):
+
+  def setUp(self):
+    super(DirectoryAPITest, self).setUp()
+    self.config = common.ProjectConfig(
+        'TEST_PROJECT', 'TEST_CLIENT_ID', 'TEST_CLIENT_SECRET', 'TEST_BUCKET')
+
+  def test_create_directory_api(self):
+    """Test the initialization of the Google Admin Directory API class."""
+    test_directory_api = directory.DirectoryAPI(self.config, mock.Mock)
+    self.assertEqual(self.config, test_directory_api._config)
+    self.assertEqual(
+        'DirectoryAPI for project: TEST_PROJECT.', str(test_directory_api))
+
+  @mock.patch.object(auth, 'CloudCredentials', autospec=True)
+  def test_create_directory_api_from_config(self, mock_creds):
+    """Test the initialization using the classmethod from_config."""
+    del mock_creds  # Unused.
+    test_directory_api = directory.DirectoryAPI.from_config(self.config)
+    self.assertEqual(self.config, test_directory_api._config)
+    self.assertEqual(
+        'DirectoryAPI for project: TEST_PROJECT.', str(test_directory_api))
+
+  def test_directory_api_insert_role(self):
+    """Test the insert_role API method for the Google Admin Directory API."""
+    mock_client = mock.Mock()
+    mock_insert = mock_client.roles.return_value.insert
+    mock_insert.return_value.execute.return_value = _FAKE_ROLE_OBJECT
+
+    test_directory_api = directory.DirectoryAPI(self.config, mock_client)
+    self.assertEqual(_FAKE_ROLE_OBJECT, test_directory_api.insert_role())
+
+  @parameterized.parameters(
+      (httplib.CONFLICT, directory.AlreadyExistsError),
+      (httplib.INTERNAL_SERVER_ERROR, directory.AlreadyExistsError),
+      (httplib.FORBIDDEN, directory.ForbiddenError),
+      (httplib.NOT_FOUND, directory.InsertionError),
+  )
+  def test_directory_api_insert_role__errors(
+      self, error_code, expected_error):
+    """Test the insert_role API method for the Google Admin Directory API."""
+    test_directory_api = directory.DirectoryAPI(self.config, mock.Mock())
+    test_directory_api._client.roles.side_effect = errors.HttpError(
+        httplib2.Response({
+            'reason': 'NOT USED', 'status': error_code}), 'NOT USED.')
+    with self.assertRaises(expected_error):
+      test_directory_api.insert_role()
+
+  def test_directory_api_insert_user(self):
+    """Test the insert_user API method for the Google Admin Directory API."""
+    mock_client = mock.Mock()
+    mock_insert = mock_client.users.return_value.insert
+    mock_insert.return_value.execute.return_value = _FAKE_USER_OBJECT
+
+    test_directory_api = directory.DirectoryAPI(self.config, mock_client)
+    self.assertEqual(
+        _FAKE_USER_OBJECT, test_directory_api.insert_user('UNUSED_PASSWORD'))
+
+  @parameterized.parameters(
+      (httplib.CONFLICT, directory.AlreadyExistsError),
+      (httplib.INTERNAL_SERVER_ERROR, directory.AlreadyExistsError),
+      (httplib.FORBIDDEN, directory.ForbiddenError),
+      (httplib.NOT_FOUND, directory.InsertionError),
+  )
+  def test_directory_api_insert_user__errors(
+      self, error_code, expected_error):
+    """Test the insert_user API method for the Google Admin Directory API."""
+    test_directory_api = directory.DirectoryAPI(self.config, mock.Mock())
+    test_directory_api._client.users.side_effect = errors.HttpError(
+        httplib2.Response({
+            'reason': 'NOT USED', 'status': error_code}), 'NOT USED.')
+    with self.assertRaises(expected_error):
+      test_directory_api.insert_user('UNUSED_PASSWORD')
+
+
+if __name__ == '__main__':
+  absltest.main()
