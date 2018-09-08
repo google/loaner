c9fd987bf6c3a2061a2698b347d50cf944540dd1
Alexandra Trant <aftrant@gmail.com>
2018-09-08 12:00:15 -0700

    Reverting Tag API.
    PiperOrigin-RevId: 212127296

--- loaner/web_app/endpoints_api.py
+++ loaner/web_app/endpoints_api.py
@@ -28,7 +28,6 @@ from loaner.web_app.backend.api import root_api
 from loaner.web_app.backend.api import search_api  # pylint: disable=unused-import
 from loaner.web_app.backend.api import shelf_api  # pylint: disable=unused-import
 from loaner.web_app.backend.api import survey_api  # pylint: disable=unused-import
-from loaner.web_app.backend.api import tag_api  # pylint: disable=unused-import
 from loaner.web_app.backend.api import user_api  # pylint: disable=unused-import
 
 ENDPOINTS_API = endpoints.api_server([root_api.ROOT_API])
--- loaner/web_app/frontend/src/app.config.ts
+++ loaner/web_app/frontend/src/app.config.ts
@@ -30,7 +30,6 @@ export interface Permissions {
   MODIFY_ROLE: string;
   MODIFY_SHELF: string;
   MODIFY_SURVEY: string;
-  MODIFY_TAG: string;
   REINDEX_SEARCH: string;
 }
 
@@ -56,7 +55,6 @@ export const APPLICATION_PERMISSIONS: Permissions = {
   MODIFY_ROLE: 'modify_role',
   MODIFY_SHELF: 'modify_shelf',
   MODIFY_SURVEY: 'modify_survey',
-  MODIFY_TAG: 'modify_tag',
   REINDEX_SEARCH: 'reindex_search',
 };
 
