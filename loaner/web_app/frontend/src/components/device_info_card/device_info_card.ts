702d2a646c9629464c6b76addae1609a3c5f6912
Googler <noreply@google.com>
2019-10-26 15:14:50 -0400

    Internal refactor
    PiperOrigin-RevId: 276872630

--- loaner/web_app/frontend/src/components/device_info_card/device_info_card.ts
+++ loaner/web_app/frontend/src/components/device_info_card/device_info_card.ts
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-
 import {Component, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {ActivatedRoute, Router} from '@angular/router';
